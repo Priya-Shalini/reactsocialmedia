3e8be297a6c537bb92036e87f03a8deca771da7a
Priya-Shalini <153379963+Priya-Shalini@users.noreply.github.com>
2025-08-22 23:01:53 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { FaBeer } from 'react-icons/fa';
+import Sidebar from "./components/Sidebar";
+import CreatePost from "./components/CreatePost";
+import PostList from "./components/PostList";
+import PostListProvider from './components/Store/post-list-store';
+
+
+function App() {
+  const [selectedTab, setSelectedTab] = useState("Create Post");
+
+  return (
+    <PostListProvider>
+    <div className="app-container">
+     <Sidebar selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
+      <div className="content">
+        <Header />
+        {selectedTab === "Home" ? <PostList /> : <CreatePost />}
+        <Footer />
+      </div>
+    </div>
+    </PostListProvider>
+  );
+}
+
+export default App;
